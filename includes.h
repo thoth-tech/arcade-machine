7f13ca888beda0670659149fde80bd318490b2c2
Sarah Gosling <sgosling@deakin.edu.au>
2022-04-13 12:40:22 +1000

    Started Selector class (currently just highlighting buttonson key input), added highlight layer to buttons class, added button highlight images

--- includes.h
+++ includes.h
@@ -13,12 +13,13 @@
 #include <regex>
 #include "splashkit.h"
 #include <string>
-#include <json/json.h>
+//#include <json/json.h>
 #include <Windows.h>
 
 #include "buttons.h"
 #include "gridlayout.h"
 #include "cdata.h"
+#include "selector.h"
 #include "splashscreen.h"
 #include "menu.h"
 #include "helper.h"
