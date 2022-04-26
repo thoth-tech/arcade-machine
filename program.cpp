f366af5f91cb0357f70360a564a33705febc0fb9
Anthony George <weareallone83@gmail.com>
2022-04-26 21:11:49 +1000

    feat(ArcadeTeam): add animation

--- program.cpp
+++ program.cpp
@@ -16,15 +16,18 @@ int main()
 
     // Open window and toggle border off.
     open_window("arcade-machine", WIDTH, HEIGHT);
-    //window_toggle_border("arcade-machine");
+    window_toggle_border("arcade-machine");
 
-    // Do we want to play the intro?
-    // Do we want to fetch new games? 
+    // Do you want to play the intros and fetch new games? 
     bool play_intro = true;
     bool load_games = true;
 
     // Play Thoth Tech intro
-    if (play_intro) Arcade.intro_thoth_tech();
+    if (play_intro) 
+    {
+        Arcade.intro_animation(Arcade.get_intro_thoth_tech(), "intro_thoth");
+        Arcade.intro_animation(Arcade.get_intro_arcade_team(), "intro_coin", "intro_start");
+    }
     // Play SplashKit intro
     if (load_games) Arcade.intro_splashkit();
     
